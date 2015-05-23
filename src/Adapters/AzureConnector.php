75281f000b3cb99e74d533117e2d6462f7bb6ced
Graham Campbell <graham@cachethq.io>
2015-05-23 15:02:49 +0100

    Updated my email

--- src/Adapters/AzureConnector.php
+++ src/Adapters/AzureConnector.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Laravel Flysystem.
  *
- * (c) Graham Campbell <graham@mineuk.com>
+ * (c) Graham Campbell <graham@cachethq.io>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -20,7 +20,7 @@ use WindowsAzure\Common\ServicesBuilder;
 /**
  * This is the azure connector class.
  *
- * @author Graham Campbell <graham@mineuk.com>
+ * @author Graham Campbell <graham@cachethq.io>
  */
 class AzureConnector implements ConnectorInterface
 {
