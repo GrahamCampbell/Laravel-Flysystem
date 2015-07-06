960d5c574f6e7be95430270c2f4d5a9f04f1f3e4
Alexandre Rosenfeld <arosenfeld@mentel.com>
2015-07-06 09:45:37 -0400

    Add config to use publicURL or internalURL in RackspaceConnector

--- src/Adapters/RackspaceConnector.php
+++ src/Adapters/RackspaceConnector.php
@@ -68,6 +68,11 @@ class RackspaceConnector implements ConnectorInterface
             throw new InvalidArgumentException('The rackspace connector requires container configuration.');
         }
 
+        $config['urlType'] = array_get($config, 'urlType', array_get($config, 'internal', false) ? 'internalURL' : 'publicURL');
+        if (!in_array($config['urlType'], ['internalURL', 'publicURL'])) {
+            throw new InvalidArgumentException('The rackspace connector requires a valid urlType.');
+        }
+
         return array_only($config, ['username', 'apiKey', 'endpoint', 'region', 'container']);
     }
 
@@ -85,7 +90,7 @@ class RackspaceConnector implements ConnectorInterface
             'apiKey'   => $auth['apiKey'],
         ]);
 
-        return $client->objectStoreService('cloudFiles', $auth['region'])->getContainer($auth['container']);
+        return $client->objectStoreService('cloudFiles', $auth['region'], $auth['urlType'])->getContainer($auth['container']);
     }
 
     /**
