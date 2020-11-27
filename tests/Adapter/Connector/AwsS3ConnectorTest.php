8f33ddb554ee31aa880de8701cb936920043c395
Graham Campbell <graham@alt-three.com>
2020-11-27 21:59:29 +0000

    Update AwsS3ConnectorTest.php

--- tests/Adapter/Connector/AwsS3ConnectorTest.php
+++ tests/Adapter/Connector/AwsS3ConnectorTest.php
@@ -155,9 +155,6 @@ class AwsS3ConnectorTest extends AbstractTestCase
         ]);
     }
 
-    /**
-     * @requires PHP < 8
-     */
     public function testConnectWithoutKey()
     {
         $connector = $this->getAwsS3Connector();
